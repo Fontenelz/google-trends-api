05e587a6c691a3861ca30f9cfa23afde6d646f24
MatheusFontenele <matheus.fontenele@icloud.com>
2025-09-23 22:45:10 -0300

    refactor: adiciona docker e estrutura arquivos

---
+++ app/schedule/__init__.py
@@ -0,0 +1,3 @@
+from .scheduler import start_scheduler, get_cache, update_trends
+
+__all__ = ["start_scheduler", "get_cache", "update_trends"]
---
+++ app/utils/__init__.py
@@ -0,0 +1,4 @@
+from .data_formater import formatar_dados
+from .scraper import fetch_trends, asession
+
+__all__ = ["formatar_dados", "fetch_trends", "asession"]
