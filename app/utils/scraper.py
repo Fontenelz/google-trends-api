31f8b522e09c7897e85baf1d16bfba05a9c90b14
MatheusFontenele <matheus.fontenele@icloud.com>
2025-09-24 00:27:50 -0300

    refactor: novo teste

--- app/utils/scraper.py
+++ app/utils/scraper.py
@@ -6,7 +6,12 @@ asession = AsyncHTMLSession()
 async def fetch_trends():
     url = "https://trends.google.com/trends/trendingsearches/daily?geo=BR"
     r = await asession.get(url)
-    await r.html.arender(timeout=20)
+    await r.html.arender(
+        timeout=20
+        keep_page=True,
+        scrolldown=0,
+        args=["--no-sandbox", "--disable-setuid-sandbox"]
+    )
 
     rows = r.html.find("tr[data-row-id]")
     data = []
