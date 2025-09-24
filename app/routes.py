a442abc0090c9496eaeb61b7d03ac177fbcb6f2f
MatheusFontenele <matheus.fontenele@icloud.com>
2025-09-24 14:27:29 -0300

    refactor: adicionar  gunicorn server railwails

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,9 @@ from .schedule import get_cache
 
 routes = Blueprint("routes", __name__)
 
-
+@routes.route("/")
+def index():
+  return jsonify({"trends": "server rodando"})
 @routes.route("/trends")
 def get_trends():
   return jsonify({"trends": get_cache()})
