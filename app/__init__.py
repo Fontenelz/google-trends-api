2911f3a09d3017bb3aaa3ead41daba619c445312
MatheusFontenele <matheus.fontenele@icloud.com>
2025-09-26 16:11:44 -0300

    refactor: corrige url cors frontend

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "https://radar-tendencias.onrender.com/"
+    "https://radar-tendencias.onrender.com"
 ]}})
 asession = AsyncHTMLSession()
 
